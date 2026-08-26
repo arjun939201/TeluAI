5ec36ba6298438985b130dc31a35a6a254b49dfe
arjun939201 <throwuse829@gmail.com>
2026-08-26 16:20:06 +0530

    feat: expose Roman-Telugu intelligence analyzer

--- app/linguistics/__init__.py
+++ app/linguistics/__init__.py
@@ -1,5 +1,6 @@
 """Telugu linguistic analysis utilities."""
 
 from .normalizer import analyze_input, normalize_roman_telugu
+from .roman_telugu import analyze_roman_telugu
 
-__all__ = ["analyze_input", "normalize_roman_telugu"]
+__all__ = ["analyze_input", "normalize_roman_telugu", "analyze_roman_telugu"]
